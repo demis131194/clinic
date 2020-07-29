0586d70b4430655f38816cd46da7fd2730b7bf33
Dzianis Katsuk <Dzianis_Katsuk@epam.com>
2020-07-29 13:02:23 +0300

    Fix sql scripts.

--- sql/postgresql/populate.sql
+++ sql/postgresql/populate.sql
@@ -1,6 +1,8 @@
 INSERT INTO users(name, surname, "user_role")
 VALUES ('Petr', 'Petrovich', 'DOCTOR'),
        ('Olga', 'Olgavna', 'NURSE'),
+       ('Valera', 'Valerievich', 'DOCTOR'),
+       ('Marina', 'Marinovna', 'NURSE'),
        ('Dmitry', 'Dmitrievich', 'DOCTOR');
 
 INSERT INTO rooms("room_name", status)
@@ -9,6 +11,9 @@ VALUES ('Room_1', 'FREE'),
        ('Room_3', 'FREE');
 
 INSERT INTO reservations("operation_name", description, "start_time", "end_time", status, "user_id", "room_id")
-VALUES ('therapy', null, '2020-05-01 04:05:06+03', '2020-05-01 07:05:06+03', 'ENDED', 1, 1),
-       ('medical procedure', 'injection of medicine', '2020-05-02 14:12:06+03', '2020-05-02 14:25:12+03', 'NEW', 2, 2),
-       ('consultation', null, '2020-05-03 17:05:06+03', '2020-05-01 17:55:00+03', 'NEW', 4, 1);
+VALUES ('therapy', null, '2020-06-01 04:05:06+03', '2020-06-01 07:05:06+03', 'ENDED', 1, 1),
+       ('medical procedure', 'injection of medicine', '2020-05-02 14:12:06+03', '2020-05-02 14:25:12+03', 'ENDED', 2, 2),
+       ('medical procedure', 'injection of medicine', '2020-05-02 14:12:06+03', '2020-05-02 14:25:12+03', 'ENDED', 4, 3),
+       ('consultation', null, '2020-07-29 17:05:06+03', '2020-08-01 17:55:00+03', 'RUNNING', 1, 1),
+       ('consultation', null, '2020-08-02 17:05:06+03', '2020-08-05 17:55:00+03', 'NEW', 1, 1),
+       ('consultation', null, '2020-07-30 17:05:06+03', '2020-08-05 17:55:00+03', 'NEW', 2, 2);
